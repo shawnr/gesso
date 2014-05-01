6280e3e149b970a137783c541c1e785dbd7b48a1
Shawn Rider <shawn@shawnrider.com>
2014-04-30 21:58:51 -0700

    Fixed editor button event listeners.
    Change-Id: Ibbfcfc6fc2d21c4253ac7b23bbb7bdf4ba5c6662

--- js/toggle_editor.js
+++ js/toggle_editor.js
@@ -4,45 +4,48 @@
 
   var useGesso = true;
 
-  $('#wiki_edit_view_secondary').prepend('<a href="#edit" id="gesso-toggle" class="btn btn-sidebar-wide"><i class="icon-edit"></i> Switch Back to Default Editor</a>');
+  $('#wiki_edit_view_secondary').prepend('<a href="" id="gesso-toggle" class="btn btn-sidebar-wide"><i class="icon-edit"></i> Switch Back to Default Editor</a>');
   $('#editor_tabs').hide();
   $('#wiki_edit_view_secondary').append('<p id="gesso-helptext" class="warning">Page Tools are unavailable when using the alternative editor. Click the button above to switch back to the default editor.</p>');
-  
-  $('#edit').on('click', function() {
+
+  $('.edit_link').on('click', function() {
+    console.log('edit button clicked')
   	$('#editor_tabs').hide();
   	$('.mceEditor').hide();
-	$('a.wiki_switch_views_link').hide();
-	$('a.toggle_views_link').hide();
+  	$('a.wiki_switch_views_link').hide();
+  	$('a.toggle_views_link').hide();
   })
 
   $('#gesso-toggle').on('click', function(){
   	if (useGesso){
+      // User is turning Gesso OFF
   		useGesso = false;
   		$('#gesso-toggle').text(' Switch to Gesso Editor');
   		$('#gesso-toggle').prepend('<i class="icon-edit"></i>');
   		// Show default stuff
   		$('#editor_tabs').show();
   		$('.mceEditor').show();
-		$('a.wiki_switch_views_link').show();
-		$('a.toggle_views_link').show();
-		// Hide enhanced stuff
+  		$('a.wiki_switch_views_link').show();
+  		$('a.toggle_views_link').show();
+  		// Hide enhanced stuff
   		$('#gesso-helptext').hide();
   		$('#cke_wiki_page_body').hide();
   		console.log(CKEDITOR.instances.wiki_page_body.getData());
   	} else {
+      // User is turning Gesso ON
   		useGesso = true;
   		$('#gesso-toggle').text(' Switch Back to Default Editor');
   		$('#gesso-toggle').prepend('<i class="icon-edit"></i>');
   		// Hide default stuff
   		$('#editor_tabs').hide();
   		$('.mceEditor').hide();
-		$('a.wiki_switch_views_link').hide();
-		$('a.toggle_views_link').hide();
-		//Show enhanced stuff
-		$('#cke_wiki_page_body').show();
+  		$('a.wiki_switch_views_link').hide();
+  		$('a.toggle_views_link').hide();
+  		//Show enhanced stuff
+		  $('#cke_wiki_page_body').show();
   		$('#gesso-helptext').show();
   	}
   })
-  
+
 
 }(this.self || global));
