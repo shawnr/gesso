ff854a3015cef0c0fae64126507f1724c8bc1eeb
Shawn Rider <shawn@shawnrider.com>
2014-05-02 15:04:15 -0700

    Added info swap to prevent losing data between editors.
    Change-Id: Ie14968d99bd65599d708e75cee395b62fd4512fe

--- js/toggle_editor.js
+++ js/toggle_editor.js
@@ -14,11 +14,12 @@
   	$('a.wiki_switch_views_link').hide();
   	$('a.toggle_views_link').hide();
   })
-
+  
   $('#gesso-toggle').on('click', function(){
   	if (useGesso){
       // User is turning Gesso OFF
   		useGesso = false;
+      
   		$('#gesso-toggle').text(' Switch to Gesso Editor');
   		$('#gesso-toggle').prepend('<i class="icon-edit"></i>');
   		// Show default stuff
@@ -29,7 +30,12 @@
   		// Hide enhanced stuff
   		$('#gesso-helptext').fadeOut("slow");
   		$('#cke_wiki_page_body').fadeOut("slow");
-  		console.log(CKEDITOR.instances.wiki_page_body.getData());
+      for ( var i in CKEDITOR.instances ){
+         var currentInstance = i;
+         break;
+      }
+      console.log('currentInstance: '+currentInstance);
+  		tinyMCE.activeEditor.setContent(CKEDITOR.instances[currentInstance].getData());
   	} else {
       // User is turning Gesso ON
   		useGesso = true;
@@ -43,6 +49,12 @@
   		//Show enhanced stuff
 		  $('#cke_wiki_page_body').fadeIn("slow");
   		$('#gesso-helptext').fadeIn("slow");
+      for ( var i in CKEDITOR.instances ){
+         var currentInstance = i;
+         break;
+      }
+      console.log('currentInstance: '+currentInstance);
+      CKEDITOR.instances[currentInstance].setData(tinyMCE.activeEditor.getContent());
   	}
   })
 
